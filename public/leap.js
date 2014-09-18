4fc6ded1896d48077e05b8120ba9345824eb4076
Alexander Halbarth <alexander@halbarth.at>
2014-09-18 22:56:21 +0200

    Added Yaw to rotate instead of gesture All gestures removed

--- public/leap.js
+++ public/leap.js
@@ -9,9 +9,9 @@
   flying = false; // used to prevent action while drone is dormant
   timeout = 400;  // used for each server publish
   speedAdjuster = 2.5; // higher number decreases action speed.  DO NOT set to less than 1
+  rotateSpeed = 0.5;
 
   var mainRoutine = function (frame) { // Runs on every frame
-    gestureHandler(frame);  // routine for handling takeoff, landing and rotations
     handPos(frame); // all other actions
   }
 
@@ -46,9 +46,15 @@
 	  } else if (handOne.fingers.length<4 && flying) {
 		land();
 	  }
-
+	  
       var pos = handOne.palmPosition;  // tracks palm of first hand
        
+	  var yaw;
+	  if(lastFrame!=null)
+		yaw = handOne.yaw(); //deadzone 35°
+	  else
+		yaw = -1;
+	  
       var xPos = pos[0]; // position of hand on x axis
       var yPos = pos[1]; // position of hand on y axis
       var zPos = pos[2]; // position of hand on z axis
@@ -59,107 +65,83 @@
       var adjYspeed = Math.abs(.4-adjY) // up/down speed
       var adjZ = zPos / 250; // -2 to 2
       var adjZspeed = Math.abs(adjZ) / speedAdjuster; // front/back speed
+	 
+	  if (yaw < -0.2 && flying) {   //rotate conterclockwise
+		speed = Math.abs(rotateSpeed * yaw);
+		counterClockwise();
+	  } else if (yaw > 0.2 && flying) {
+		speed = Math.abs(rotateSpeed * yaw);
+		clockwise();
+	  } else {	//yaw or fly (Never both!!)
+		  if (adjX < 0 && flying) { // flying set in takeoff() and land() to prevent actions while drone landed
+			stopped = false;
+			$(".left").attr({id: 'highlight'})
+			$(".right").attr({id: ''})
+			setTimeout(function (){
+			  return faye.publish("/drone/move", {
+				  action: 'left',
+				  speed: adjXspeed
+					})
+			}, timeout);
+		  } else if (adjX > 0 && flying) {
+			stopped = false;
+			$(".right").attr({id: 'highlight'})
+			$(".left").attr({id: ''})
+			setTimeout(function (){
+			  return faye.publish("/drone/move", {
+				action: 'right',
+				speed: adjXspeed
+				  })
+			}, timeout);
+		  }
 
-      if (adjX < 0 && flying) { // flying set in takeoff() and land() to prevent actions while drone landed
-        stopped = false;
-        $(".left").attr({id: 'highlight'})
-        $(".right").attr({id: ''})
-        setTimeout(function (){
-          return faye.publish("/drone/move", {
-    	      action: 'left',
-    	      speed: adjXspeed
- 			    })
-        }, timeout);
-      } else if (adjX > 0 && flying) {
-        stopped = false;
-        $(".right").attr({id: 'highlight'})
-        $(".left").attr({id: ''})
-        setTimeout(function (){
-          return faye.publish("/drone/move", {
-      	    action: 'right',
-      	    speed: adjXspeed
-   			  })
-        }, timeout);
-      }
+		  if (adjY > 0.4 && flying) {
+			stopped = false;
+			$(".up").attr({id: 'highlight'})
+			$(".down").attr({id: ''})
+			setTimeout(function (){
+			  return faye.publish("/drone/move", {
+				action: 'up',
+				speed: adjYspeed
+			  })
+			}, timeout/2);
+		  } else if (adjY < 0.4 && flying) {
+			stopped = false;
+			$(".down").attr({id: 'highlight'})
+			$(".up").attr({id: ''})
+			setTimeout(function (){
+			  return faye.publish("/drone/move", {
+				action: 'down',
+				speed: adjYspeed
+				  })
+			}, timeout/2);
+		  }
 
-      if (adjY > 0.4 && flying) {
-        stopped = false;
-        $(".up").attr({id: 'highlight'})
-        $(".down").attr({id: ''})
-        setTimeout(function (){
-          return faye.publish("/drone/move", {
-      	    action: 'up',
-      	    speed: adjYspeed
-          })
-        }, timeout/2);
-      } else if (adjY < 0.4 && flying) {
-        stopped = false;
-        $(".down").attr({id: 'highlight'})
-        $(".up").attr({id: ''})
-        setTimeout(function (){
-          return faye.publish("/drone/move", {
-      	    action: 'down',
-      	    speed: adjYspeed
-   			  })
-        }, timeout/2);
-      }
-
-      if (adjZ < 0 && flying) {
-        stopped = false;
-        $(".front").attr({id: 'highlight'})
-        $(".back").attr({id: ''})
-        setTimeout(function (){
-          return faye.publish("/drone/move", {
-      	    action: 'front',
-      	    speed: adjZspeed
-   			  })
-        }, timeout/3);
-      } else if (adjZ > 0 && flying) {
-        stopped = false;
-        $(".back").attr({id: 'highlight'})
-        $(".front").attr({id: ''})
-        setTimeout(function (){
-          return faye.publish("/drone/move", {
-      	    action: 'back',
-      	    speed: adjZspeed
-   			  })
-        }, timeout/3);
-      }
+		  if (adjZ < 0 && flying) {
+			stopped = false;
+			$(".front").attr({id: 'highlight'})
+			$(".back").attr({id: ''})
+			setTimeout(function (){
+			  return faye.publish("/drone/move", {
+				action: 'front',
+				speed: adjZspeed
+				  })
+			}, timeout/3);
+		  } else if (adjZ > 0 && flying) {
+			stopped = false;
+			$(".back").attr({id: 'highlight'})
+			$(".front").attr({id: ''})
+			setTimeout(function (){
+			  return faye.publish("/drone/move", {
+				action: 'back',
+				speed: adjZspeed
+				  })
+			}, timeout/3);
+		  }
+	  }
     }
   }
 
-  var gestureHandler = function (frame) { // handles rotation
-    var gestures = frame.gestures;
-    if (gestures && gestures.length > 0) {
-      stopped = false;
-      for( var i = 0; i < gestures.length; i++ ) {
-        var gesture = gestures[i];
-        if (gesture.type === 'circle' && gesture.state === 'start' && flying) {
-          gesture.pointable = frame.pointable(gesture.pointableIds[0]);
-          direction = gesture.pointable.direction;
-          if(direction) {
-            var normal = gesture.normal;
-            clockwisely = Leap.vec3.dot(direction, normal) > 0;
-            if(clockwisely) {
-              return clockwise();
-            } else {
-              return counterClockwise();
-            }
-          }
-        } else {
-          if ( gesture.type === 'keyTap' ) { // motion that looks like clicking a mouse controls takeoff and landing
-            if (flying) { // flying set in the takeoff() function and ensures that drone will land while flying and takeoff while dormant
-              land();
-            } else {
-              takeoff();
-            }
-          }
-        }
-      }
-    }
-  };
-
-  speed = 0.5; // used for rotation speed
   var counterClockwise = function () {
     $(".counterClockwise").attr({id: 'highlight'})
     $(".clockwise").attr({id: ''})
